46d209106a199fce9662f401e5c4107d771948fa
Bùi Chí Thanh <93216434+thanhbui1234@users.noreply.github.com>
2025-04-01 00:06:14 +0700

    perfoment libsary

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
-import _ from 'lodash'
+import pick from 'lodash/pick'
 
 // provip
 export const getInfoData = <T>(params: { fields: Array<keyof T>; object: T }): Partial<T> => {
   const { fields, object } = params
-  return _.pick(object, fields)
+  return pick(object, fields)
 }
